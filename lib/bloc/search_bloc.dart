95b0074a24eadae9f58d4989a69f5fe7e5f04d5c
Shreyas Sarve <i.made.jarvis@gmail.com>
2022-04-07 15:43:32 +0530

    ---final synchronization--

--- lib/bloc/search_bloc.dart
+++ lib/bloc/search_bloc.dart
@@ -15,33 +15,45 @@ class NoSearchYet extends SearchState {}
 
 class BookFound extends SearchState {}
 
+class ErrorOccured extends SearchState {}
+
 class SearchBloc extends Bloc<SearchEvent, SearchState> {
-  late List<Book> _books;
+  List<Book> _books = [];
   SearchBloc(SearchState initialState) : super(initialState) {
     on<SearchEvent>(_makeSearch);
   }
-  _makeSearch(SearchEvent event, emit) async* {
+  _makeSearch(SearchEvent event, emit) async {
     CollectionReference _collectionReference =
         FirebaseFirestore.instance.collection("books");
     final snapshot = await _collectionReference
         .where("bookName", isEqualTo: event.search)
         .get();
+    print(snapshot.size);
+
     if (snapshot.size == 0) {
+      print("inside If");
       emit(NoBooksFound());
     } else {
+      _books.clear();
       for (int i = 0; i < snapshot.docs.length; i++) {
         Map<String, dynamic> json = {
           "id": "",
           "bookName": "",
-          "authorName": []
+          "authorName": List<String>
         };
         json["bookName"] = snapshot.docs[i]["bookName"];
         json["id"] = snapshot.docs[i].id.toString();
         json["authorName"] = snapshot.docs[i]["authorName"];
         _books.add(Book.fromJson(json));
-        print("Called");
+        Book _book = Book(
+            id: snapshot.docs[i].id,
+            bookName: snapshot.docs[i]["bookName"],
+            authorName: snapshot.docs[i]["authorName"]);
+        print(_book.authorName);
       }
+      emit(BookFound());
     }
-    emit(BookFound());
   }
+
+  List<Book> get result => _books;
 }
