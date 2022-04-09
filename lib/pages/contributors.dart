d4983bcde7ddd0938bafe0a2fe008f8a85bb71cf
Shreyas Sarve <i.made.jarvis@gmail.com>
2022-04-09 11:36:59 +0530

    modified bloc

---
+++ lib/pages/contributors.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class Contributors extends StatelessWidget {
+  const Contributors({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          Container(
+            child: TextFormField(),
+          ),
+        ],
+      ),
+    );
+  }
+}
--- lib/static/author.dart
+++
@@ -1,6 +0,0 @@
-import 'package:library_managment/classes/book.dart';
-
-Book influencePeople =
-    Book(authorName: ["Dale Carnige"], bookName: "How To Win", id: "PD01");
-
-Map<String, Book> bookOfAuthor = {"Dale Carnige": influencePeople};
